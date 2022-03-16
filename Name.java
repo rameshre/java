68d7d58c2cb60db8341811785105b1fc1aa2a7bf
rameshre <zycon1mac@gmail.com>
2022-03-16 09:45:52 +0530

    first commit

---
+++ Name.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Name
+{
+public static void main(String args[])
+{
+Scanner obj=new Scanner(System.in);
+System.out.println("Enter the First Name\n");
+String fname=obj.next();
+Scanner obj1=new Scanner(System.in);
+System.out.println("Enter the Last Name\n");
+String lname=obj1.next();
+System.out.println(fname+" "+lname);
+}
+}
---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Sum
+{
+public static void main(String args[])
+{
+Scanner obj=new Scanner(System.in);
+System.out.println("Enter the First Number\n");
+int a=obj.nextInt();
+System.out.println("Enter the Second Number\n");
+int b=obj.nextInt();
+int s=a+b;
+System.out.println("Sum="+s);
+}
+}
