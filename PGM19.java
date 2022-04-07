db509e56c16b89b7c840e49075d7f828e49b1249
rameshre <zycon1mac@gmail.com>
2022-04-07 14:31:35 +0530

    upto 19

--- PGM19.java
+++ PGM19.java
@@ -6,5 +6,21 @@ public static void main(String args[])
 Scanner obj=new Scanner(System.in);
 System.out.println("Enter the String\n");
 String n=obj.nextLine();
+int s=n.length();
+boolean ispalindrome=true;
+for(int i=0;i<s/2;i++)
+{
+if(n.charAt(i)==n.charAt(s-i-1))
+continue;
+else
+{
+ispalindrome=false;
+break;
+}
+}
+if(ispalindrome)
+System.out.println("The string "+n+" is palindrome\n");
+else
+System.out.println("The string "+n+" is not palindrome\n");
 }
 }
